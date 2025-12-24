0fe6b14715b3a6f9b0836207f0ba11302711ba0d
RevaLohe <revalohe@yahoo.com>
2025-12-25 01:09:00 +0530

    CI Pipeline Fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     baseURL: 'https://restful-booker.herokuapp.com',
-    headless:false
+    headless:!!process.env.CI
     
   },
 
