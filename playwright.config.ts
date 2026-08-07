9c5c744a46d6ae8fcc2c9d3f07df38d40280e221
RevaLohe <revalohe@yahoo.com>
2026-08-07 13:29:28 +1000

    Add Allure reports with GitHub Pages publishing in CI.

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI
+    ? [
+        ['html', { open: 'never', outputFolder: 'playwright-report' }],
+        ['allure-playwright', { outputFolder: 'allure-results' }],
+        ['list'],
+      ]
+    : [
+        ['html'],
+        ['allure-playwright', { outputFolder: 'allure-results' }],
+      ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
